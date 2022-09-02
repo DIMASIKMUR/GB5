20b7ffd3e030d7e8d2016dfabe23bba0b6b412a5
DIMASIK <mdsy98@mail.ru>
2022-09-02 23:12:15 +0300

    final

--- Zadacha34/Program.cs
+++ Zadacha34/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] chislo = new int [7];
+int n = chislo.Length;
+
+FillArray(chislo);
+OutputArrayString(chislo);
+CounterEvenNumber(chislo);
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+}
+
+void CounterEvenNumber(int[] number)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (number[i] % 2 == 0)
+        {
+            count += 1;
+        }
+    }
+    Console.WriteLine($" -> колличество четных чисел в массиве равно: {count}");
+}
+
+void OutputArrayString(int[] arrayString)
+{
+    string stringArray = "[";
+
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        stringArray += $"{arrayString[i]}, ";
+    }
+    int n = stringArray.Length;
+    stringArray = stringArray.Remove(n - 2, 2);
+    stringArray += "]";
+    Console.Write($"Массив: {stringArray}");
+}
